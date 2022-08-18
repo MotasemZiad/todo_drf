5757005328ae9653f02cff2e6834e4e5d4b43262
MotasemZiad <moatasem.abunema@gmail.com>
2022-08-18 14:40:46 +0300

    CRUD operations

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.api_overview, name='api-overview'),
     path('task-list/', views.task_list, name='task-list'),
+    path('task-detail/<int:id>/', views.task_detail, name='task-detail'),
+    path('task-create/', views.task_create, name='task-create'),
+    path('task-update/<int:id>/', views.task_update, name='task-update'),
+    path('task-delete/<int:id>/', views.task_delete, name='task-delete'),
 ]
--- api/views.py
+++ api/views.py
@@ -10,10 +10,10 @@ from .serializers import TaskSerializer
 def api_overview(request):
     api_urls = {
         'List': '/task-list/',
-        'Detail View': '/task-detail/<str:pk>/',
+        'Detail View': '/task-detail/<int:id>/',
         'Create': 'task-create/',
-        'Update': 'task-update/<str:pk>/',
-        'Delete': 'task-delete/<str:pk>/',
+        'Update': 'task-update/<int:id>/',
+        'Delete': 'task-delete/<int:id>/',
     }
     return Response(api_urls)
 
@@ -23,3 +23,34 @@ def task_list(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def task_detail(request, id):
+    task = Task.objects.get(id=id)
+    serializer = TaskSerializer(task)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def task_create(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def task_update(request, id):
+    task = Task.objects.get(id=id)
+    serializer = TaskSerializer(instance=task, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def task_delete(request, id):
+    task = Task.objects.get(id=id)
+    task.delete()
+    return Response("Task deleted Successfully!")
