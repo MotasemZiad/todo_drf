aa96e69087fe9f61f58e7d3ff0fc65da9a6af511
MotasemZiad <moatasem.abunema@gmail.com>
2022-08-21 13:42:02 +0300

    update the API admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'completed')
+    exclude = ('created_at',)
+
+
+admin.site.register(Task, TaskAdmin)
