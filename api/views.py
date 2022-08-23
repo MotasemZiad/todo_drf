d70a376b676c6812bb823bc69df2090c53b0dc82
MotasemZiad <moatasem.abunema@gmail.com>
2022-08-23 17:39:52 +0300

    finish the full app using Ajax

--- api/views.py
+++ api/views.py
@@ -21,7 +21,7 @@ def api_overview(request):
 
 @api_view(['GET'])
 def task_list(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('-id')
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
 
